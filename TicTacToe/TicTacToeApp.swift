58f42f8f0d77f4f9a2a6b85019ae17323511e55b
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-01-25 14:31:18 +0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Turgut Eren Aksu on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
