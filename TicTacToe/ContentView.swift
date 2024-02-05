802eb27a4c7cfeac1a834d51298c3cce3dbd5445
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-02-05 08:09:16 +0300

    Game result condition

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -62,10 +62,20 @@ struct ContentView: View {
 		
 		return movePosition
 	}
+	
+	
+	func winCheck(for player: Player, in moves: [Move?]) -> Bool {
+		let winPatterns: Set<Set<Int>> = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+		return true
+	}
 }
+
+
 enum Player{
 	case human, computer
 }
+
+
 struct Move {
 	let player: Player
 	let boardIndex: Int
@@ -73,6 +83,8 @@ struct Move {
 		return player == .human ? "xmark" : "circle"
 	}
 }
+
+
 #Preview {
 	ContentView()
 }
