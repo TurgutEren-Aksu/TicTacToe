2f6c29f6e1e20535a0fb6061afebbbbfbde89c4e
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-02-06 07:37:43 +0300

    Game result condition

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -66,6 +66,8 @@ struct ContentView: View {
 	
 	func winCheck(for player: Player, in moves: [Move?]) -> Bool {
 		let winPatterns: Set<Set<Int>> = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+		let playerMove = moves.compactMap{ $0 }.filter{ $0.player == player }
+		let playerPosition = Set(playerMove.map{$0.boardIndex})
 		return true
 	}
 }
